846b2d9bbae8de2568e61f70bffda21525097fa7
prasadsourya <prasadsourya@gmail.com>
2020-04-14 11:51:27 +0530

    LRUTest class creted with test cases

--- LRUcache.py
+++ LRUcache.py
@@ -1,4 +1,4 @@
-class LRU :
+class LRUcache :
     def __init__(self):
         self.input=[]
     def put(self):
@@ -7,3 +7,51 @@ class LRU :
         pass
     def get_cache(self):
         pass
+
+
+class LRUTest:
+    def __init__(self):
+        self.LRUcacheobject = LRUcache()
+        self.count =0
+
+    def put_test(self):
+        self.LRUcacheobject.put(7)
+        try:
+            assert self.LRUcacheobject.input == [7]
+            self.count += 1
+        except Exception as e:
+            print("put function doesn't work.")
+        
+        
+
+    def get_test(self):
+        value = self.LRUcacheobject.get()
+        try:
+            assert value == [7]
+            self.count += 1
+        except Exception as e:
+            print("put function doesn't work.")
+        
+        
+
+    def get_cache_test(self):
+        value1 = self.LRUcacheobject.get_cache()
+        try:
+            assert value1 == self.LRUcacheobject.input
+            self.count += 1
+        except Exception as e:
+            print("put function doesn't work.")
+        
+
+
+
+def main():
+    LRUTestobject = LRUTest()
+    LRUTestobject.put_test()
+    LRUTestobject.get_test()
+    LRUTestobject.get_cache_test()
+    if LRUTestobject.count == 3:
+        print ("All Test cases passed")
+
+if __name__ == "__main__":
+    main()
